8f5a14465f2fcfd2fa8d2d63562a41f5bcf9603f
Dissssy <ethanconaway@yahoo.com>
2020-12-03 14:35:55 -0500

    post base
    base code for every challenge

---
+++ code.py
@@ -0,0 +1,6 @@
+#open the file and parse it into a list of strings on newlines
+text_file = open("input.txt", "r")
+lines = text_file.read().split('\n')
+
+#every input text file has an empty newline at the end, delete it
+lines.pop(-1)
