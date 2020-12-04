965b1655b0f5f862402c057101b7221b01e79327
Dissssy <ethanconaway@yahoo.com>
2020-12-04 16:24:13 -0500

    day 4
    challenge day 4, this was kinda hard

---
+++ 04/1/code.py
@@ -0,0 +1,21 @@
+#open the file and parse it into a list of strings on newlines
+text_file = open("input.txt", "r")
+lines = text_file.read().split('\n\n')
+
+count = 0
+for i in range(0, len(lines)):
+    lines[i] = lines[i].replace('\n', ' ')
+    lines[i] = lines[i].split(' ')
+
+lines[len(lines)-1].pop(len(lines[len(lines)-1])-1)
+for i in range(0, len(lines)):
+    j = 0
+    while j < len(lines[i]):
+        if(lines[i][j].find('cid') > -1):
+            lines[i].pop(j)
+            j += 1
+        j += 1
+    if(len(lines[i]) == 7):
+        count += 1
+
+print(count)
---
+++ 04/2/code.py
@@ -0,0 +1,94 @@
+import re
+#open the file and parse it into a list of strings on newlines
+text_file = open("input.txt", "r")
+lines = text_file.read().split('\n\n')
+
+def check(thisline):
+    for j in range(0, len(thisline)):
+        thisline[j] = thisline[j].split(':')
+        if(thisline[j][0] == 'byr'):
+            if(1920 > int(thisline[j][1]) or int(thisline[j][1]) > 2002):
+                print(thisline[j])
+                print('invalid byr')
+                return False
+        elif(thisline[j][0] == 'iyr'):
+            if(2010 > int(thisline[j][1]) or int(thisline[j][1]) > 2020):
+                print(thisline[j])
+                print('invalid iyr')
+                return False
+        elif(thisline[j][0] == 'eyr'):
+            if(2020 > int(thisline[j][1]) or int(thisline[j][1]) > 2030):
+                print(thisline[j])
+                print('invalid eyr')
+                return False
+        elif(thisline[j][0] == 'hgt'):
+            if(thisline[j][1].find('cm') > -1):
+                if(150 > int(thisline[j][1].replace('cm', '')) or int(thisline[j][1].replace('cm', '')) > 193):
+                    print(thisline[j])
+                    print('invalid hgt cm')
+                    return False
+            elif(thisline[j][1].find('in') > -1):
+                if(59 > int(thisline[j][1].replace('in', '')) or int(thisline[j][1].replace('in', '')) > 76):
+                    print(thisline[j])
+                    print('invalid hgt in')
+                    return False
+            else:
+                print(thisline[j])
+                print('invalid hgt something else')
+                return False
+        elif(thisline[j][0] == 'hcl'):
+            if not(re.search(r'^#(?:[0-9a-fA-F]{1,2}){3}$', thisline[j][1])):
+                print(thisline[j])
+                print('invalid hcl')
+                return False
+        elif(thisline[j][0] == 'ecl'):
+            if('amb blu brn gry grn hzl oth'.find(thisline[j][1]) == -1):
+                print(thisline[j])
+                print('invalid ecl')
+                return False 
+        elif(thisline[j][0] == 'pid'):
+            if(len(thisline[j][1]) != 9):
+                print(thisline[j])
+                print('invalid pid')
+                return False
+        else:
+            print(thisline[j])
+            print('invalid something else')
+            return False
+    print(thisline)
+    print('everything valid')
+    return True
+
+count = 0
+for i in range(0, len(lines)):
+    lines[i] = lines[i].replace('\n', ' ')
+    lines[i] = lines[i].split(' ')
+
+lines[len(lines)-1].pop(len(lines[len(lines)-1])-1)
+i = 0
+while i < len(lines):
+    j = 0
+    while j < len(lines[i]):
+        if(lines[i][j].find('cid') > -1):
+            lines[i].pop(j)
+            j += 1
+        j += 1
+    if(len(lines[i]) == 7):
+        pass
+        #count += 1
+    else:
+        lines.pop(i)
+        i -= 1
+    i += 1
+
+for i in range(0, len(lines)):
+    if check(lines[i]):
+        count += 1
+        #print(True)
+    else:
+        pass
+        #print(False)
+    #print(lines[i])
+
+print(count)
+
