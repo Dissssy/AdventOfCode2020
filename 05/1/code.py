6e11b6bcbbe2a2e594687d09f4f306f5b38e163d
Dissssy <ethanconaway@yahoo.com>
2020-12-05 02:16:34 -0500

    day FIVE BAYBEE

---
+++ 05/1/code.py
@@ -0,0 +1,25 @@
+#open the file and parse it into a list of strings on newlines
+text_file = open("input.txt", "r")
+lines = text_file.read().split('\n')
+
+#every input text file has an empty newline at the end, delete it
+lines.pop(-1)
+
+def truemod(numerator, denominator):
+    modulo = [0]*2
+    modulo[1] = int(numerator % denominator)
+    modulo[0] = int((numerator - modulo[1]) / denominator)
+    return modulo
+
+decimal = [0]*len(lines)
+for i in range(0, len(lines)):
+    lines[i] = lines[i].replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1')
+    for j in range(0, len(lines[i])):
+        decimal[i] += (int(lines[i][j]) % 10) * pow(2, 9 - j)
+
+largest = 0
+for i in range(0, len(decimal)):
+    if(decimal[i] > largest):
+        largest = decimal[i]
+
+print(largest)
---
+++ 05/2/code.py
@@ -0,0 +1,26 @@
+#open the file and parse it into a list of strings on newlines
+text_file = open("input.txt", "r")
+lines = text_file.read().split('\n')
+
+#every input text file has an empty newline at the end, delete it
+lines.pop(-1)
+
+def truemod(numerator, denominator):
+    modulo = [0]*2
+    modulo[1] = int(numerator % denominator)
+    modulo[0] = int((numerator - modulo[1]) / denominator)
+    return modulo
+
+decimal = [0]*len(lines)
+for i in range(0, len(lines)):
+    lines[i] = lines[i].replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1')
+    for j in range(0, len(lines[i])):
+        decimal[i] += (int(lines[i][j]) % 10) * pow(2, 9 - j)
+
+sort = sorted(decimal)
+final = 0
+for i in range(0, len(sort) - 2):
+    if not (sort[i] == sort[i + 1] - 1 and sort[i + 1] - 1 == sort[i + 2] - 2):
+        final = sort[i]
+
+print(final + 1)
