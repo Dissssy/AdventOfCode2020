10f7d290c26c64d4b1c1a4141208caad43c18532
Dissssy <ethanconaway@yahoo.com>
2020-12-09 16:30:15 -0500

    day 9

---
+++ 09/1/code.py
@@ -0,0 +1,21 @@
+#open the file and parse it into a list of strings on newlines
+text_file = open("input.txt", "r")
+lines = text_file.read().split('\n')
+
+#every input text file has an empty newline at the end, delete it
+lines.pop(-1)
+
+def isvalid(i):
+    for j in range(i - 25, i):
+        for l in range(j + 1, i):
+            if(lines[j] + lines[l] == lines[i]):
+                return True
+    return False
+
+for i in range(0, len(lines)):
+    lines[i] = int(lines[i])
+
+for i in range(25, len(lines)):
+    if not isvalid(i):
+        print(lines[i])
+        break
---
+++ 09/2/code.py
@@ -0,0 +1,34 @@
+#open the file and parse it into a list of strings on newlines
+text_file = open("input.txt", "r")
+lines = text_file.read().split('\n')
+
+#every input text file has an empty newline at the end, delete it
+lines.pop(-1)
+
+def isvalid(i):
+    for j in range(i - 25, i):
+        for l in range(j + 1, i):
+            if(lines[j] + lines[l] == lines[i]):
+                return True
+    return False
+
+def check():
+    for i in range(2, len(lines)):
+        for j in range(0, len(lines) - i):
+            check = [None]*i
+            sum = 0
+            for l in range(j, j + i):
+                check[l - j] = lines[l]
+            for l in range(0, len(check)):
+                sum += check[l]
+            if(sum == invalid):
+                return(min(check) + max(check))
+
+for i in range(0, len(lines)):
+    lines[i] = int(lines[i])
+
+for i in range(25, len(lines)):
+    if not isvalid(i):
+        invalid = lines[i]
+
+print(check())
